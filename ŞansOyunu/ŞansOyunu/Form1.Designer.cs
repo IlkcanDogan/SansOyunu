2f21075edd01852add59716658d3ef52f1f6968c
İlkcan Doğan <40579186+IlkcanDogan@users.noreply.github.com>
2019-03-29 00:15:23 +0300

    Add files via upload

---
+++ ŞansOyunu/ŞansOyunu/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace ŞansOyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBasla = new System.Windows.Forms.Button();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnBasla
+            // 
+            this.btnBasla.Location = new System.Drawing.Point(16, 196);
+            this.btnBasla.Name = "btnBasla";
+            this.btnBasla.Size = new System.Drawing.Size(347, 51);
+            this.btnBasla.TabIndex = 0;
+            this.btnBasla.Text = "Başla";
+            this.btnBasla.UseVisualStyleBackColor = true;
+            this.btnBasla.Click += new System.EventHandler(this.btnBasla_Click);
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.lbl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl1.Location = new System.Drawing.Point(14, 31);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(99, 108);
+            this.lbl1.TabIndex = 1;
+            this.lbl1.Text = "0";
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.lbl2.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl2.Location = new System.Drawing.Point(144, 31);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(99, 108);
+            this.lbl2.TabIndex = 2;
+            this.lbl2.Text = "0";
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.lbl3.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl3.Location = new System.Drawing.Point(264, 31);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(99, 108);
+            this.lbl3.TabIndex = 3;
+            this.lbl3.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(9, 151);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(182, 42);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Puanınız: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(173, 151);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 42);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "30";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(375, 259);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.btnBasla);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBasla;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.Label lblPuan;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ŞansOyunu/ŞansOyunu/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ŞansOyunu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int puan = 30;
+
+        private void btnBasla_Click(object sender, EventArgs e)
+        {
+            int random1, random2, random3;
+            Random rnd = new Random();
+            random1 = rnd.Next(0,10);
+            random2 = rnd.Next(0, 10);
+            random3 = rnd.Next(0, 10);        
+            lbl1.Text = random1.ToString();
+            lbl2.Text = random2.ToString();
+            lbl3.Text = random3.ToString();
+
+
+
+            if (random1 == 7 && random2 == 7 && random3 == 7)
+            {
+                puan += 100;
+                label2.Text = puan.ToString();
+            }
+            else if (random1 == 7 && random2 == 7)
+            {
+                puan += 50;
+                label2.Text = puan.ToString();
+            }
+            else if (random1 == 7 && random3 == 7)
+            {
+                puan += 40;
+                label2.Text = puan.ToString();
+            }
+            else if (random2 == 7 && random3 == 7)
+            {
+                puan += 30;
+                label2.Text = puan.ToString(); ;
+            }
+            else if (random1 == 7 && random3 == 7)
+            {
+                puan += 20;
+                label2.Text = puan.ToString();
+            }
+            else if(random1 == 7)
+            {
+                puan += 10;
+                label2.Text = puan.ToString();
+            }
+            else
+            {
+                puan -= 5;
+                label2.Text = puan.ToString();
+            }
+            if(puan <= 0)
+            {
+                MessageBox.Show("Oyun bitti");
+                label2.Text = "30";
+                puan = 30;
+                lbl1.Text = "0";
+                lbl2.Text = "0";
+                lbl3.Text = "0";
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+            
+        }
+    }
+}
